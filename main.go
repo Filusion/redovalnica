fb66708b9ac9b7db8e77e79a6418a8ca70818a78
Filusion <filip.vicoroski11@gmail.com>
2025-11-23 15:37:53 +0100

    Add main using urave/cli and redovalnica package

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Filusion/redovalnica/redovalnica"
+	"github.com/urfave/cli/v3"
+)
+
+func main() {
+	app := &cli.Command{
+		Name:  "redovalnica-cli",
+		Usage: "Simple grade register CLI using package redovalnica",
+		Flags: []cli.Flag{
+			&cli.IntFlag{
+				Name:  "stOcen",
+				Value: 5,
+				Usage: "Minimum number of grades required for positive status",
+			},
+			&cli.IntFlag{
+				Name:  "minOcena",
+				Value: 0,
+				Usage: "Minimum allowed grade",
+			},
+			&cli.IntFlag{
+				Name:  "maxOcena",
+				Value: 10,
+				Usage: "Maximum allowed grade",
+			},
+		},
+		Action: func(ctx context.Context, c *cli.Command) error {
+			// read CLI flags
+			stOcen := c.Int("stOcen")
+			minOc := c.Int("minOcena")
+			maxOc := c.Int("maxOcena")
+
+			// create a new Redovalnica
+			r := redovalnica.New(stOcen, minOc, maxOc)
+
+			// add some sample students
+			r.AddStudent("63230454", "Filip", "Vichoroski", []int{10, 10, 9, 9, 8})
+			r.AddStudent("63230423", "Angel", "Karali", []int{8, 9, 6, 7, 8})
+			r.AddStudent("63233333", "Peter", "Griffin", []int{5, 5, 4, 7, 6})
+
+			// print all grades
+			fmt.Println(r.IzpisVsehOcen())
+			fmt.Println("----- KONČNI USPEH -----")
+			fmt.Print(r.IzpisiKoncniUspeh())
+
+			// example: add a new grade
+			if err := r.DodajOceno("63230454", 9); err != nil {
+				fmt.Printf("DodajOceno error: %v\n", err)
+			} else {
+				fmt.Println("Added grade to 63230454")
+			}
+
+			fmt.Println("After adding a grade:")
+			fmt.Println(r.IzpisVsehOcen())
+
+			return nil
+		},
+	}
+
+	if err := app.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
